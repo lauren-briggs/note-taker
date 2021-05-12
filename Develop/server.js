259626c0b064a213047fcde7698396fb7addb2e0
lauren-briggs <laurenbriggs41@gmail.com>
2021-05-13 09:35:20 +1000

    added comments on functionality

--- Develop/server.js
+++ Develop/server.js
@@ -1,26 +1,25 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-// const bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 const app = express();
 const PORT = 3000;
 
 app.use(express.static('public'));
-// app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.json());
 
-
+// Initial get index.html
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
+// Initial get notes.html
 app.get('/notes', function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 
-// Get existing notes
+// Get existing notes - read db.json file
 app.get('/api/notes', (req, res) => {
     const data = fs.readFileSync("db/db.json", "utf8");
     res.status(200).send(JSON.parse(data));
@@ -30,44 +29,38 @@ app.get('/api/notes', (req, res) => {
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
     const data = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
-
     // Set new notes id
     newNote.id = data.length;
-    console.log(newNote.id);
-
     // Pushing new note onto data const
     data.push(newNote);
     // Writing db.json file with new data
     fs.writeFileSync("db/db.json", JSON.stringify(data));
-
     res.status(201).send(`Note created: "${newNote.title}`);
     console.log(`Note created: ${newNote.title}`);
 })
 
+// Get note by id to render on click
 app.get("/api/notes/:id", function (req, res) {
     const data = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
     console.log("req.body.id: " + req.body.id);
-    console.log(data[req.params.id]);
     res.json(data[req.params.id]);
 });
 
-
+// Get note by id for deletion
 app.delete("/api/notes/:id", function (req, res) {
     const data = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
 
     let id = req.params.id.toString();
-    console.log(`ID to be deleted: ${id}`);
+    console.log(`Note ID to be deleted: ${id}`);
 
-    console.log(data);
+    // Filtering the notes against the condition of matching the selected ID
     let filteredData = data.filter(function (note) {
         return note.id != req.params.id;
     });
 
-    console.log(filteredData);
-
     fs.writeFileSync("db/db.json", JSON.stringify(filteredData));
-    console.log("note deleted");
-    res.send(JSON.parse(filteredData));
+    console.log("Note deleted");
+    res.send(filteredData);
 })
 
 app.listen(PORT, () => {
