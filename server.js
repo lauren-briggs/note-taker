74a466878bcb125181b7cd37be8e993779c94dc6
lauren-briggs <laurenbriggs41@gmail.com>
2021-05-13 18:22:20 +1000

    refactored code

--- server.js
+++ server.js
@@ -3,32 +3,31 @@ const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser')
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
 // Initial get index.html
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 // Initial get notes.html
 app.get('/notes', function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
-
 
 // Get existing notes - read db.json file
 app.get('/api/notes', (req, res) => {
-    const data = fs.readFileSync("db/db.json", "utf8");
+    const data = fs.readFileSync("./db/db.json", "utf8");
     res.status(200).send(JSON.parse(data));
 })
 
 // Post new notes
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
-    const data = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+    const data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     // Set new notes id
     newNote.id = data.length;
     // Pushing new note onto data const
@@ -41,14 +40,14 @@ app.post('/api/notes', (req, res) => {
 
 // Get note by id to render on click
 app.get("/api/notes/:id", function (req, res) {
-    const data = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+    const data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     console.log("req.body.id: " + req.body.id);
     res.json(data[req.params.id]);
 });
 
 // Get note by id for deletion
 app.delete("/api/notes/:id", function (req, res) {
-    const data = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+    const data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
     let id = req.params.id.toString();
     console.log(`Note ID to be deleted: ${id}`);
